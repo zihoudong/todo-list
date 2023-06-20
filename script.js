880aa24ef189a803d0eb1a242b15de1d943b83fb
zihoudong <137188143+zihoudong@users.noreply.github.com>
2023-06-20 23:19:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,97 @@
-document.getElementById('new-task-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+// 从localStorage中获取待办事项
+var todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-    var taskName = document.getElementById('new-task-name').value;
-    var subtaskName = document.getElementById('new-subtask-name').value;
-    var taskNote = document.getElementById('new-task-note').value;
-    var taskDate = document.getElementById('new-task-date').value;
-
-    var taskList = document.getElementById('task-list');
-
-    var taskElement = document.createElement('div');
-    taskElement.className = 'task';
-
-    var checkboxElement = document.createElement('input');
-    checkboxElement.type = 'checkbox';
-    checkboxElement.addEventListener('change', function() {
-        if (checkboxElement.checked) {
-            taskNameElement.style.textDecoration = 'line-through';
-            taskNameElement.style.color = '#aaa';
-        } else {
-            taskNameElement.style.textDecoration = 'none';
-            taskNameElement.style.color = '#000';
-        }
+// 渲染待办事项
+function renderTodos() {
+    var todoList = document.getElementById('todo-list');
+    todoList.innerHTML = '';
+    var sortSelect = document.getElementById('sort-select');
+    var searchInput = document.getElementById('search-input');
+    var dates = [...new Set(todos.map(todo => todo.date))]; // 获取所有的日期
+    if (sortSelect.value === 'asc') {
+        dates.sort((a, b) => new Date(a) - new Date(b)); // 日期升序排序
+    } else {
+        dates.sort((a, b) => new Date(b) - new Date(a)); // 日期降序排序
+    }
+    dates.forEach(function(date) {
+        var dateItem = document.createElement('div');
+        var dateTitle = document.createElement('h2');
+        dateTitle.textContent = date;
+        dateTitle.addEventListener('click', function() {
+            var todoItems = dateItem.getElementsByClassName('todo-item');
+            for (var i = 0; i < todoItems.length; i++) {
+                var display = todoItems[i].style.display;
+                todoItems[i].style.display = display === 'none' ? 'block' : 'none';
+            }
+        });
+        dateItem.appendChild(dateTitle);
+        todos.filter(todo => todo.date === date && todo.text.includes(searchInput.value)).forEach(function(todo, index) {
+            var todoItem = document.createElement('div');
+            todoItem.className = 'todo-item';
+            var todoText = document.createElement('p');
+            todoText.textContent = todo.text + ' - ' + todo.description;
+            if (todo.completed) {
+                todoText.style.textDecoration = 'line-through';
+            }
+            todoText.addEventListener('click', function() {
+                todo.completed = !todo.completed;
+                saveTodos();
+                renderTodos();
+            });
+            var deleteButton = document.createElement('button');
+            deleteButton.textContent = '删除';
+            deleteButton.addEventListener('click', function(event) {
+                event.stopPropagation();
+                todos.splice(index, 1);
+                saveTodos();
+                renderTodos();
+            });
+            var editButton = document.createElement('button');
+            editButton.textContent = '编辑';
+            editButton.addEventListener('click', function(event) {
+                event.stopPropagation();
+                var editText = prompt('请输入新的待办事项', todo.text);
+                var editDate = prompt('请输入新的日期', todo.date);
+                var editDescription = prompt('请输入新的描述', todo.description);
+                if (editText !== null) todo.text = editText;
+                if (editDate !== null) todo.date = editDate;
+                if (editDescription !== null) todo.description = editDescription;
+                saveTodos();
+                renderTodos();
+            });
+            todoItem.appendChild(todoText);
+            todoItem.appendChild(deleteButton);
+            todoItem.appendChild(editButton);
+            dateItem.appendChild(todoItem);
+        });
+        todoList.appendChild(dateItem);
     });
-    taskElement.appendChild(checkboxElement);
+}
 
-    var taskNameElement = document.createElement('div');
-    taskNameElement.textContent = taskName;
-    taskElement.appendChild(taskNameElement);
+// 保存待办事项到localStorage
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
 
-    var subtaskElement = document.createElement('div');
-    subtaskElement.className = 'subtask';
-    subtaskElement.textContent = subtaskName;
-    taskElement.appendChild(subtaskElement);
-
-    var noteElement = document.createElement('div');
-    noteElement.className = 'note';
-    noteElement.textContent = taskNote;
-    taskElement.appendChild(noteElement);
-
-    var dateElement = document.createElement('div');
-    dateElement.className = 'date';
-    dateElement.textContent = taskDate;
-    taskElement.appendChild(dateElement);
-
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', function() {
-        taskList.removeChild(taskElement);
+document.getElementById('add-todo').addEventListener('click', function() {
+    var todoInput = document.getElementById('todo-input');
+    var dateInput = document.getElementById('date-input');
+    var descriptionInput = document.getElementById('description-input');
+    todos.push({
+        text: todoInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+        completed: false
     });
-    taskElement.appendChild(deleteButton);
-
-    taskList.appendChild(taskElement);
-
-    document.getElementById('new-task-name').value = '';
-    document.getElementById('new-subtask-name').value = '';
-    document.getElementById('new-task-note').value = '';
-    document.getElementById('new-task-date').value = '';
+    saveTodos();
+    renderTodos();
+    todoInput.value = '';
+    dateInput.value = '';
+    descriptionInput.value = '';
 });
+
+document.getElementById('sort-select').addEventListener('change', renderTodos);
+document.getElementById('search-input').addEventListener('input', renderTodos);
+
+// 初始渲染待办事项
+renderTodos();
